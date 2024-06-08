d3fbcdf8c7b0b91107b72fe8c766bc6ffac3c2c8
GERALD <gonzalesgm.work@gmail.com>
2024-06-08 22:14:27 +0800

    Initial Commit

---
+++ ItemAPI/ItemAPI/Models/ItemContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ItemAPI.Models
+{
+    public class ItemContext : DbContext
+    {
+        public ItemContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
