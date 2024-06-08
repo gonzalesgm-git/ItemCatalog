0e61be087f998a4463759d6671f28254315a41a6
GERALD <gonzalesgm.work@gmail.com>
2024-06-08 23:23:02 +0800

    add component and services

--- ItemAPI/ItemUI/src/app/app.module.ts
+++ ItemAPI/ItemUI/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemDetailsComponent } from './item-details/item-details.component';
+import { ItemDetailFormComponent } from './item-details/item-detail-form/item-detail-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemDetailsComponent,
+    ItemDetailFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ ItemAPI/ItemUI/src/app/item-details/item-detail-form/item-detail-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-detail-form',
+  templateUrl: './item-detail-form.component.html',
+  styles: ``
+})
+export class ItemDetailFormComponent {
+
+}
