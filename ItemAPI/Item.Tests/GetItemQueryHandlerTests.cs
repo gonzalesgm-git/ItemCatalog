02d898c2b137713b27eb9ed81e7ae3ca329d7faf
GERALD <gonzalesgm.work@gmail.com>
2024-06-09 14:07:52 +0800

    add tests

---
+++ ItemAPI/Item.Tests/GetItemQueryHandlerTests.cs
@@ -0,0 +1,35 @@
+﻿using Item.Application.Queries;
+using Item.Infrastructure.Repositories;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Item.Tests
+{
+    public class GetItemQueryHandlerTests
+    {
+        [Fact]
+        public async Task Handle_Should_Return_Items()
+        {
+            //Arrange
+            MockData mockData = new MockData();
+            var mockItemRepo = new Mock<IItemRepository>();
+            mockItemRepo.Setup(x => x.GetItemsAsync())
+                .ReturnsAsync(mockData.GetItems());
+
+            // Act
+            var repo = new GetItemsQueryHandler(mockItemRepo.Object);
+            var request = new GetItemsQuery();
+            var results = await repo.Handle(request, CancellationToken.None);
+
+            // Assert
+            Assert.NotNull(results);
+            Assert.Equal(mockData.GetItems().Count(), results.Count());
+            mockItemRepo.Verify(x => x.GetItemsAsync(), Times.Once());
+        }
+    }
+}
---
+++ ItemAPI/Item.Tests/MockData.cs
@@ -0,0 +1,14 @@
+﻿namespace Item.Tests
+{
+    public class MockData
+    {
+        public List<Domain.Models.Item> GetItems()
+        {
+            Random rnd = new Random();
+            IEnumerable<Domain.Models.Item> items = Enumerable.Repeat(
+                new Domain.Models.Item { Id = rnd.Next(1, 100), Manufacturer = "mfg1", Name = "Item1", Price = 0, Quantity = 1 }, 10);
+
+            return items.ToList();
+        }
+    }
+}
